6044ea99fc508ae14ee6c3c6aaf8a91ace8e71e6
Andrew Boschee <andrew.j.boschee@outlook.com>
2019-08-08 09:20:32 -0500

    Add files via upload

---
+++ MLR.R
@@ -0,0 +1,33 @@
+library(MASS)
+library(ISLR)
+
+names(Boston)
+?Boston
+plot(medv~lstat, Boston)
+fit1=lm(medv~lstat, data=Boston)
+fit1
+summary(fit1)
+abline(fit1, col='red')
+names(fit1)
+confint(fit1)
+predict(fit1, data.frame(lstat=c(5,10,15)), interval='confidence')
+
+
+fit2=lm(medv~lstat+age, data=Boston)
+summary(fit2)
+fit3=lm(medv~.,Boston)
+summary(fit3)
+par(mfrow=c(2,2))
+plot(fit3)
+fit4=update(fit3,~.-age-indus)
+summary(fit4)
+fit5=lm(medv~lstat*age,Boston)
+summary(fit5)
+fit6=lm(medv~lstat + I(lstat^2),Boston)
+summary(fit6)
+attach(Boston)
+par(mfrow=c(1,1))
+plot(medv~lstat)
+points(lstat,fitted(fit6),col='red',pch=20)
+fit7=lm(medv~poly(lstat,4))
+points(lstat,fitted(fit7),col='blue',pch=20)
